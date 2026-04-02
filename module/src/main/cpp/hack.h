e12508016e4a6969c1ec4699ee3d1b37fdc5cb53
illyasev27-netizen <illyasev27@gmail.com>
2026-04-02 22:28:25 +0300

    Update hack.h

--- module/src/main/cpp/hack.h
+++ module/src/main/cpp/hack.h
@@ -1,12 +1,10 @@
-//
-// Created by Perfare on 2020/7/4.
-//
-
 #ifndef ZYGISK_IL2CPPDUMPER_HACK_H
 #define ZYGISK_IL2CPPDUMPER_HACK_H
 
 #include <stddef.h>
 
+// Объявляем обе функции, чтобы они были доступны в main.cpp
 void hack_prepare(const char *game_data_dir, void *data, size_t length);
+void hack_start(const char *game_data_dir);
 
 #endif //ZYGISK_IL2CPPDUMPER_HACK_H
