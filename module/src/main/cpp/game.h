c088ed628403f9079c726f7fe2df07d0bf5aada6
illyasev27-netizen <illyasev27@gmail.com>
2026-05-02 11:36:57 +0300

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,7 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.wemadeconnect.aos.lostdgl"
 #define GamePackageName "com.mobile.legends"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
