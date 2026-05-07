505ef846df80077dbb3e5333a78e4406b9232212
illyasev27-netizen <illyasev27@gmail.com>
2026-05-07 17:06:45 +0300

    Update il2cpp-api-functions.h

--- module/src/main/cpp/il2cpp-api-functions.h
+++ module/src/main/cpp/il2cpp-api-functions.h
@@ -2,8 +2,8 @@
 #define DO_API_NO_RETURN(r, n, p) DO_API(r,n,p)
 #endif
 
-DO_API(const char*, il2cpp_method_get_name, (const MethodInfo* method))
-DO_API(const char*, il2cpp_class_get_name, (Il2CppClass* klass))
+DO_API(const Il2CppCodeRegistration*, il2cpp_get_code_registration, ())
+DO_API(const Il2CppMetadataRegistration*, il2cpp_get_metadata_registration, ())
 DO_API(int, il2cpp_init, (const char* domain_name));
 DO_API(int, il2cpp_init_utf16, (const Il2CppChar * domain_name));
 DO_API(void, il2cpp_shutdown, ());
